d8251d99fd7d37527053ab84a293e8de13631937
Jay Callicott <jrcallicott@gmail.com>
2026-03-08 08:37:49 -0500

    feat: add Cloudflare Pages compatibility

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: 'http', hostname: 'template.localhost', port: '8888', pathname: '/**' },
       { protocol: 'https', hostname: '**.decoupled.website', pathname: '/**' },
